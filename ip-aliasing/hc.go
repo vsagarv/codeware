9d982da7f091f380f667d0bcbd59a3851c5f71f6
Vijaya Sagar Vinnakota <vijayasagar@gmail.com>
2018-01-12 19:14:46 +0530

    added waitgroup sync

--- ip-aliasing/hc.go
+++ ip-aliasing/hc.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"sync"
 )
 
 const	MAX_CONNS	int = 10000
@@ -13,19 +14,28 @@ const	MAX_CONNS	int = 10000
 // - https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts/
 
 func	main() {
+	var wg sync.WaitGroup
+
+	wg.Add(MAX_CONNS)
+
 	for i := 0; i < MAX_CONNS; i++ {
 		go func(id int) {
+			defer wg.Done()
+
 			var hc = &http.Client{
 				Timeout: time.Second * 10,
 			}
 
-			response, err := hc.Get("localhost:80")
+			response, err := hc.Get("http://localhost:80")
 
 			if err != nil {
 				fmt.Println("err: ", id, err)
 			} else {
+				fmt.Println(response.Status, response.ContentLength)
 				defer response.Body.Close()
 			}
 		}(i)
 	}
+
+	wg.Wait()
 }
